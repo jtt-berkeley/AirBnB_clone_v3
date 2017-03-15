256e42ce9f581c5d9d08d5315ddada2ce941228e
Jianqin Wang <jianqinwang94@gmail.com>
2017-03-15 13:05:08 -0700

    fix sed command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,5 +17,5 @@ echo "<!DOCTYPE html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '39 i\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
