817863e50d2edf91c019b03db4373427bc8d3309
Jianqin Wang <jianqinwang94@gmail.com>
2017-03-15 13:01:04 -0700

    use libraries

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+file to practice use of Fabric
+"""
+import tarfile
+import time
+import os
+
+
+def do_pack():
+    timestr = time.strftime("%Y%m%d%H%M%S")
+    try:
+        os.stat("./version/")
+    except:
+        os.mkdir("./version/")
+    tar = tarfile.open("./version/web_static_"+timestr+".tgz", "w:gz")
+    tar.add("./web_static/")
