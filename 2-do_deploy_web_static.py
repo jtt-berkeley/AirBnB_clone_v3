01c5a019c30e504e51f59cdc5a42e9832e23367c
Jianqin Wang <jianqinwang94@gmail.com>
2017-03-15 13:53:14 -0700

    fix pep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,11 +5,11 @@ script that distributes archive to webservers
 import os
 from fabric.api import env
 from fabric.operations import run, put
-
-
 env.hosts = ['52.90.98.156', '52.207.85.204']
+
+
 def do_deploy(archive_path):
-    if (os.path.exists(archive_path) == False):
+    if (os.path.exists(archive_path) is False):
         return False
     try:
         for ip_add in env.hosts:
