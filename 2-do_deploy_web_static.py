646ff920bd86471e917a698d9225731d8dfdbcfd
Jianqin Wang <jianqinwang94@gmail.com>
2017-03-15 14:07:42 -0700

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ script that distributes archive to webservers
 """
 import os
 from fabric.api import env
-from fabric.operations import run, put
+from fabric.operations import run, put, sudo
 env.hosts = ['52.90.98.156', '52.207.85.204']
 
 
@@ -14,6 +14,19 @@ def do_deploy(archive_path):
     try:
         for ip_add in env.hosts:
             put(archive_path, "/tmp/")
-        return True
+            with (settings(host_string=ip_add)):
+                new_arch = archive_path.split("/")
+                new_folder = ("/data/web_static/releases/"+ new_arch[-1][:-4])
+                cmd = "mkdir " + new_folder
+                sudo(cmd)
+                cmd = "tar -xzf /tmp/"+ new_arch[-1] + " -C " + new_folder
+                sudo(cmd)
+                cmd = "mv " + new_folder + "/web_static/* " + new_folder
+                sudo(cmd)
+                cmd = "rm -rf " + new_folder + "/web_static"
+                sudo('rm -rf /data/web_static/current')
+                cmd = "ln -s " + new_folder + " /data/web_static?current"
+                sudo(cmd)
+        return True:
     except:
         return False
