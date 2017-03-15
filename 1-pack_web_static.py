4952f3935556af03bd45567896487b506ce58a29
Jianqin Wang <jianqinwang94@gmail.com>
2017-03-15 13:19:31 -0700

    fix folder name

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,17 +5,17 @@ file to practice use of Fabric
 import tarfile
 import time
 import os
-
+from subprocess import call
 
 def do_pack():
     timestr = time.strftime("%Y%m%d%H%M%S")
     try:
-        os.stat("./version/")
+        os.stat("./versions")
     except:
-        os.mkdir("./version/")
+        os.mkdir("./versions")
     try:
-        tar = tarfile.open("./version/web_static_"+timestr+".tgz", "w:gz")
-        tar.add("./web_static/")
-        return (os.getcwd()+"/version/web_static_"+timestr+".tgz")
+        tar = tarfile.open("./versions/web_static_"+timestr+".tgz", "w:gz")
+        tar.add("./web_static")
+        return (os.getcwd()+"/versions/web_static_"+timestr+".tgz")
     except:
         return None
