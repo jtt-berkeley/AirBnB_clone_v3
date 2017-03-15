eb62e96a5690cc0401dc0674981151782cb671d7
Jianqin Wang <jianqinwang94@gmail.com>
2017-03-15 13:04:03 -0700

    change order of symbolic link creation flags

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,7 +15,7 @@ echo "<!DOCTYPE html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '39 i\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
