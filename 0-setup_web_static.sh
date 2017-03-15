f2a0c16dab699de5d63abf1b979e9bafcc87a92a
Jianqin Wang <jianqinwang94@gmail.com>
2017-03-15 11:07:02 -0700

    fix sed command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,5 @@ sudo mkdir /data/web_static/releases/test/
 echo 'Test html' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '39 i/location /hbnb_static {\n\t\talias /data/web_static/current;\n\t}/' /etc/nginx/sites-enabled/default
+sudo sed -i '39 i\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
