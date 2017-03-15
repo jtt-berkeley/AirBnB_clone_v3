886cf34da447a99d185d17a2fe6e5c7c2fac9a34
Jianqin Wang <jianqinwang94@gmail.com>
2017-03-15 13:52:43 -0700

    preliminary push>

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script that distributes archive to webservers
+"""
+import os
+from fabric.api import env
+from fabric.operations import run, put
+
+
+env.hosts = ['52.90.98.156', '52.207.85.204']
+def do_deploy(archive_path):
+    if (os.path.exists(archive_path) == False):
+        return False
+    try:
+        for ip_add in env.hosts:
+            put(archive_path, "/tmp/")
+        return True
+    except:
+        return False
