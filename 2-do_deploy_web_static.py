d8e82d2af5fbe347c29c4289e278653ded8d194e
Jianqin Wang <jianqinwang94@gmail.com>
2017-03-15 17:52:33 -0700

    include do_pack

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,9 +3,23 @@
 script that distributes archive to webservers
 """
 import os
-from fabric.api import env, run
+from fabric.api import *
 from fabric.operations import run, put, sudo
 env.hosts = ['52.90.98.156', '52.207.85.204']
+
+
+def do_pack():
+    timestr = time.strftime("%Y%m%d%H%M%S")
+    try:
+        os.stat("./versions")
+    except:
+        os.mkdir("./versions")
+    try:
+        tar = tarfile.open("./versions/web_static_"+timestr+".tgz", "w:gz")
+        tar.add("./web_static")
+        return (os.getcwd()+"/versions/web_static_"+timestr+".tgz")
+    except:
+        return None
 
 
 def do_deploy(archive_path):
@@ -24,5 +38,4 @@ def do_deploy(archive_path):
             run("ln -s {} /data/web_static/current".format(new_folder))
         return True
     except:
-        print("some fail")
         return False
