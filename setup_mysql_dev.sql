232257328bf1df09c9d442c12c5e11a9546559eb
rdsim8589 <rdsim8589@github.com>
2017-03-18 22:54:17 +0000

    changed comments to add a resource if root is unable to grant privilage

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,8 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 DROP USER IF EXISTS 'hbnb_dev'@'localhost';
 -- creates a user with user:hbnb_dev@localhost with pass hbnb_dev_pwd
 CREATE USER 'hbnb_dev'@'localhost'IDENTIFIED BY 'hbnb_dev_pwd';
--- gives power to grant privileges to root user
-UPDATE mysql.user SET Grant_priv='Y', Super_priv='Y' WHERE User='root';
-FLUSH PRIVILEGES;
+-- if user root@localhost unable to grant priv
+-- http://stackoverflow.com/questions/21714869/error-1044-42000-access-denied-for-root-with-all-privileges
 -- grant user hbnb_dev all privileges to database hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* to 'hbnb_dev'@'localhost'
