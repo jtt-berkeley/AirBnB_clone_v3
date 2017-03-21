87a206709c679dfac850d3551c9bf2f06b69fc5b
Richard Sim <rdsim8589@gmail.com>
2017-03-21 12:59:18 -0700

    updated db_storage methods

--- models/engine/db_engine.py
+++ models/engine/db_engine.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sqlalchemy import create_engine
-from os import getenvb
+from sqlalchemy.orm import sessionmaker
+from os import getenv
 
 
 class DBstorage:
@@ -8,21 +9,27 @@ class DBstorage:
     __session = None
     def __init__(self):
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            getenvb('HBNB_MYSQL_USER'),
-            getenvb('HBNB_MYSQL_PWD'),
-            getenvb('HBNB_MYSQL_HOST'),
-            getenvb(' HBNB_MYSQL_DB')))
+            getenv('HBNB_MYSQL_USER'),
+            getenv('HBNB_MYSQL_PWD'),
+            getenv('HBNB_MYSQL_HOST'),
+            getenv(' HBNB_MYSQL_DB')))
 
     def all(self, cls=None):
         orm_objects = {}
         if cls:
             cls_obj = self.__session.query(cls).all()
-            
         else:
             for i in ['User', 'State', 'Amenity', 'Place', 'Review']:
                 self.__session.query(i).all()
         self.__session.
     def new(self, obj):
+        self.__session.add(obj)
     def save(self):
+        self.__session.commit()
     def delete(self, obj=None):
+        if !obj:
+            self.__session.delete(obj)
     def reload(self):
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
