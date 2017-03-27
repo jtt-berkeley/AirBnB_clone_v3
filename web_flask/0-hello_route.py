6aac5c4d761f8e188f4fe0fd56fb43ea42b0a116
Anne <anne.cognet@holbertonschool.com>
2017-03-27 22:19:47 +0000

    first commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_hbnb():
+    """flask hello world"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port="5000")
