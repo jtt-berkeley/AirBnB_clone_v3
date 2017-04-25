ba809f6abe00b33a6cd487bfc46d0c7f91ab92e4
Incitatous <dcianberlin@gmail.com>
2017-04-25 13:47:40 -0700

    ordered imports

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import jsonify, app
+from flask import app, jsonify
 from models import storage
 """
 create a route /status on the object
